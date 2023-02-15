5030801560c66de0fccf282ee27f0e30eeee0835
BrandoPinto <72404805+BrandoPinto@users.noreply.github.com>
2023-02-15 16:47:07 -0500

    Vista de Login

---
+++ FRONTEND/front/src/login.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+export const Login =()=>{
+    return(
+        <section>
+        <div class="signup">
+  <div class="signup-connect">   
+  </div>
+  <div class="signup-classic">
+    <h3>JP NET</h3>
+    <h2>Iniciar Sesión</h2>
+    <form class="form" action="">
+        <input class="username" type="text" placeholder="Nombre de usuario" />
+        <input class="username"type="password" placeholder="Contraseña" />
+
+      <button type="submit" class="btn">Ingresar</button>
+    </form>
+  </div>
+</div>
+       
+    </section>
+    
+    
+         
+    )
+}
